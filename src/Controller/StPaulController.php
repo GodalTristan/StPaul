ad8b3bd2b5cfba21c1c30a8a8c7fe2f9db3a9f38
unknown <17tgodal@ESTRANSUP.LOCAL>
2019-03-07 09:52:28 +0100

    V1.2

--- src/Controller/StPaulController.php
+++ src/Controller/StPaulController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Sejour;
+use App\Repository\SejourRepository;
 
 class StPaulController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
-    {
-        return $this->render('home/indexOld.php', [
-            'controller_name' => 'StPaulController',
+    public function index(SejourRepository $repo)
+    {   
+        $sejours = $repo->findAll();
+        return $this->render('home/index.html.twig', [
+            'sejours' => $sejours,
         ]);
     }
 }
